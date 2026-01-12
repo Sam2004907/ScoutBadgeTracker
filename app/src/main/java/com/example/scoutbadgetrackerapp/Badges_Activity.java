96bfb3e283423b1ded29d385d7645e82b2b7e524
samwi <samwilmer2004@gmail.com>
2026-01-12 17:51:03 +0000

    Inclusion of image buttons that change layout on SelectedBadges_Activity

--- app/src/main/java/com/example/scoutbadgetrackerapp/Badges_Activity.java
+++ app/src/main/java/com/example/scoutbadgetrackerapp/Badges_Activity.java
@@ -1,14 +1,59 @@
 package com.example.scoutbadgetrackerapp;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.annotation.Nullable;
 
 public class Badges_Activity extends Activity{
+    ImageButton imgbtnBadge1, imgbtnBadge2, imgbtnBadge3, imgbtnBadge4;
+    Intent activity;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_badges);
+        imgbtnBadge1 = findViewById(R.id.imgbtnBadge1);
+        imgbtnBadge2 = findViewById(R.id.imgbtnBadge2);
+        imgbtnBadge3 = findViewById(R.id.imgbtnBadge3);
+        imgbtnBadge4 = findViewById(R.id.imgbtnBadge4);
+        imgbtnBadge1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", (String) imgbtnBadge1.getContentDescription());
+                activity = new Intent(Badges_Activity.this, SelectedBadges_Activity.class);
+                activity.putExtra("key", (String) imgbtnBadge1.getContentDescription());
+                startActivity(activity);
+
+            }
+        });
+        imgbtnBadge2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", (String) imgbtnBadge2.getContentDescription());
+                activity = new Intent(Badges_Activity.this, SelectedBadges_Activity.class);
+                activity.putExtra("key", (String) imgbtnBadge2.getContentDescription());
+                startActivity(activity);
+
+            }
+        });
+        imgbtnBadge3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", (String) imgbtnBadge3.getContentDescription());
+                activity = new Intent(Badges_Activity.this, SelectedBadges_Activity.class);
+                activity.putExtra("key", (String) imgbtnBadge3.getContentDescription());
+                startActivity(activity);
+
+            }
+        });
+        imgbtnBadge4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", (String) imgbtnBadge4.getContentDescription());
+                activity = new Intent(Badges_Activity.this, SelectedBadges_Activity.class);
+                activity.putExtra("key", (String) imgbtnBadge4.getContentDescription());
+                startActivity(activity);
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/scoutbadgetrackerapp/SelectedBadges_Activity.java
+++ app/src/main/java/com/example/scoutbadgetrackerapp/SelectedBadges_Activity.java
@@ -1,15 +1,45 @@
 package com.example.scoutbadgetrackerapp;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import java.util.Objects;
+
 public class SelectedBadges_Activity extends Activity{
+    ImageView imgBadge;
+    TextView txtTitle;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectedbadge);
+        imgBadge = findViewById(R.id.imgBadge);
+        txtTitle = findViewById(R.id.txtTitle);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("key");
+            //The key argument here must match that used in the other activity
+            Log.d("Activity", value);
+            if(Objects.equals(value, "activity_sc_activitycenterservice")){
+                imgBadge.setImageResource(R.drawable.activity_sc_activitycenterservice);
+                txtTitle.setText("Activity Center Service");
+            }else if(Objects.equals(value, "activity_sc_air_or_sea_nav")){
+                imgBadge.setImageResource(R.drawable.activity_sc_air_or_sea_nav);
+                txtTitle.setText("Air or Sea Navigation");
+            }else if(Objects.equals(value, "activity_sc_air_researcher")){
+                imgBadge.setImageResource(R.drawable.activity_sc_air_researcher);
+                txtTitle.setText("Air Researcher");
+            }else if(Objects.equals(value, "activity_sc_air_spotter")){
+                imgBadge.setImageResource(R.drawable.activity_sc_air_spotter);
+                txtTitle.setText("Air Spotter");
+            }
+        }
     }
 
     @Override
